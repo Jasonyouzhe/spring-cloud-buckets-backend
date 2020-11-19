5d14d81cf160081f49b9ab5f333d1bf7084f4463
zhangdengpan <zhangdengpan@kedacom.com>
2020-11-19 13:02:47 +0800

    EurekaServer

--- spring-cloud-buckets-starter/src/main/java/com/spring/cloud/buckets/Application.java
+++ spring-cloud-buckets-starter/src/main/java/com/spring/cloud/buckets/Application.java
@@ -3,7 +3,10 @@ package com.spring.cloud.buckets;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+@EnableEurekaServer
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
