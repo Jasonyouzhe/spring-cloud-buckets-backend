452983a183806dbe9d514f6e15e458ad3c411635
zhangdengpan <zhangdengpan@kedacom.com>
2020-11-26 19:24:02 +0800

    断路器

--- spring-cloud-buckets-starter/src/main/java/com/spring/cloud/buckets/Application.java
+++ spring-cloud-buckets-starter/src/main/java/com/spring/cloud/buckets/Application.java
@@ -3,7 +3,9 @@ package com.spring.cloud.buckets;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 
 @EnableEurekaServer
